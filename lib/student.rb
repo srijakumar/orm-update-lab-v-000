0582baf415771b4da30712918f21575790ee7b1a
Srija S <srijakumar7@gmail.com>
2018-08-05 22:16:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,4 +73,9 @@ def self.find_by_name(name)
    end.first
  end
 
+ def update
+   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+   DB[:conn].execute(sql, self.name, self.grade, self.id)
+ end
+
 end
